41275dad1114ffbd3d50e7d94547c3eb26ea9deb
danqyin <dyin@student.unimelb.edu.au>
2016-10-26 23:31:04 +1100

    Refilter numbers, ), and - special char as included in stopwords

--- nlp_tokenizer.py
+++ nlp_tokenizer.py
@@ -18,7 +18,8 @@ import operator
 pickle_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
 stop_words = set(stopwords.words('english'))
-stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}', '%']) # remove it if you need punctuation
+stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}',
+                   '%', '-', "),", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]) # remove it if you need punctuation
 
 # results = search("ultraviolet rays[MeSH Terms] AND osteoporosis[MeSH Terms]")
 def initialize_data(topic):
